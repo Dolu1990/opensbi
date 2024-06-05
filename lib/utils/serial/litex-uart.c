a0293d5ec57173ec8be180b5ead8c96b1c0153be
Dolu1990 <charles.papon.90@gmail.com>
2024-06-05 16:39:53 +0200

    lib: utils: serial: litex: fix io access

--- lib/utils/serial/litex-uart.c
+++ lib/utils/serial/litex-uart.c
@@ -20,6 +20,10 @@
 #define UART_REG_EV_PENDING	4
 #define UART_REG_EV_ENABLE	5
 
+#define UART_EV_TX	0x1
+#define UART_EV_RX	0x2
+
+
 /* clang-format on */
 
 static volatile u32 *uart_base;
@@ -38,14 +42,17 @@ static void litex_uart_putc(char ch)
 {
 	while (get_reg(UART_REG_TXFULL));
 	set_reg(UART_REG_RXTX, ch);
+	set_reg(UART_REG_EV_PENDING, UART_EV_TX);
 }
 
 static int litex_uart_getc(void)
 {
 	if (get_reg(UART_REG_RXEMPTY))
 		return -1;
-	else
-		return get_reg(UART_REG_RXTX);
+
+	u32 rx = get_reg(UART_REG_RXTX);
+	set_reg(UART_REG_EV_PENDING, UART_EV_RX);
+	return rx;
 }
 
 static struct sbi_console_device litex_console = {
